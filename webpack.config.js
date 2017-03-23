c7ec310d1bf98626d49d75c402caca90cabaa565
vladosby <vladislavn.kievski@gmail.com>
2017-03-24 00:06:38 +0300

    Add watching

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,11 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         library: '[name]'
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000,
+        ignored: /node_modules/
     }
 };
