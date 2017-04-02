8f7e79c6d3c640110274df2d21ff90ba76060842
vladosby <vladislavn.kievski@gmail.com>
2017-04-02 23:49:54 +0300

    Add using of env variables. Run build with command webpack --env.mode=prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,51 @@
 'use strict';
 var path = require('path');
 
-module.exports = {
-    context: path.resolve(__dirname, 'app'),
-    entry: {
-        app: './app',
-        home: './home'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        library: '[name]'
-    },
-    watch: true,
-    watchOptions: {
-        aggregateTimeout: 300,
-        poll: 1000,
-        ignored: /ignored_directory/
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                options: {
-                    presets: ['env']
+module.exports = (env)=> {
+    return {
+        context: path.resolve(__dirname, 'app'),
+        entry: {
+            app: './app',
+            home: './home'
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+            library: '[name]'
+        },
+        watch: env.mode === 'dev',
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000,
+            ignored: /ignored_directory/
+        },
+        devtool: env.mode === 'dev' ? 'source-map' : '',
+        module: {
+            rules: [
+                {
+                    test: /.js$/,
+                    exclude: /(node_modules)/,
+                    loader: 'babel',
+                    options: {
+                        presets: ['env']
+                    }
                 }
+            ]
+        },
+        resolve: {
+            modules: [path.resolve(__dirname, 'app'), path.resolve(__dirname, 'node_modules')],
+            extensions: ['.js'],
+            alias: {
+                delayedAlert: path.resolve(__dirname, 'vendor/old_script/lib/delayed-alert')
             }
-        ]
-    },
-    resolve: {
-        modules: [path.resolve(__dirname, 'app'), path.resolve(__dirname, 'node_modules')],
-        extensions: ['.js'],
-        alias: {
-            delayedAlert: path.resolve(__dirname, 'vendor/old_script/lib/delayed-alert')
+        },
+        resolveLoader: {
+            modules: [path.resolve(__dirname, 'node_modules')],
+            extensions: ['.js'],
+            moduleExtensions: ['-loader']
+        },
+        externals: {
+            jquery: 'jQuery'
         }
-    },
-    resolveLoader: {
-        modules: [path.resolve(__dirname, 'node_modules')],
-        extensions: ['.js'],
-        moduleExtensions: ['-loader']
-    },
-    externals: {
-        jquery: 'jQuery'
-    }
+    };
 };
