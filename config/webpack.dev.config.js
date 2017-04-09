91421bf392d2f2fd9b873743e48b29d4e648a106
vladosby <vladislavn.kievski@gmail.com>
2017-04-09 16:13:55 +0300

    Add webpack dev server

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,17 +1,23 @@
 const commonConfig = require('./webpack.common.config');
 const webpackMerge = require('webpack-merge');
+var path = require('path');
 
 module.exports = (env)=> {
     const MODE = env !== undefined ? env : 'dev';
 
     return webpackMerge(commonConfig(MODE), {
-        watch: true,
-        watchOptions: {
-            aggregateTimeout: 300,
-            poll: 1000,
-            ignored: /ignored_directory/
-        },
-        devtool: 'source-map'
+        devtool: 'source-map',
+        devServer: {
+            publicPath: '/dist/',
+            compress: true,
+            contentBase: [ path.join(__dirname, "../app"), path.join(__dirname, "../public"), path.join(__dirname, "../vendor")],
+            port: 3000,
+            overlay: true,
+            watchOptions: {
+                aggregateTimeout: 300,
+                poll: 1000
+            }
+        }
     });
 
 };
